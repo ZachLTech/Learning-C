989c7c13c8744451963f7585599d05faefd67e53
ZachLTech <zlopezzl2021@gmail.com>
2023-02-24 17:16:01 -0500

    new file:   Work/TempConverterThroughtTxt/StepsForCreation/Steps.md 	modified:   Work/TempConverterThroughtTxt/temps_C.txt 	modified:   Work/TempConverterThroughtTxt/txtediting 	modified:   Work/TempConverterThroughtTxt/txtediting.c

---
+++ Work/TempConverterThroughtTxt/StepsForCreation/Steps.md
@@ -0,0 +1,40 @@
+## Variable/Pointer Declaration
+
+- Declare your 2 file pointers (one for the provided temps_F txt file and one for the temps_C txt file that will be made through the program)
+
+- set the tempF file pointer equal to a statement that opens the "temps_F.txt" file and sets it to read mode
+- set the tempC file pointer equal to a statement that targets the "temps_C.txt" file (which should automatically be made when the final code is run) and sets it to write mode
+
+- declare the double variables needed for the program 
+    - 2 arrays set to 10 empty spaces (one for the F temps | one for the C temps) (10 empty spaces since there are 10 temperatures you're working with)
+    - 2 for the sum of all C and F temps
+    - 2 for the C and F averages
+
+## For Loop to read, convert, and write each temperature | also to add all the F and C temps to one variable
+
+- Open the for loop with these parameters:
+    - a new integer set equal to 0 (I'll be calling it i standing for integer)
+    - a conditional that passes if i is <= 9
+    - a command that adds 1 to i if the conditional is passed 
+    - (each parameter separated by semicolens in the for parameter parenthesis)
+
+# In the For Loop:
+
+- a line with fscanf command that scans a temperature from file pointed variable temps_F and assigns it to your fahrenheit array in position i (when the loop is running, i is replaced with a proper position between 0 and 9 (including 0 and 9) since there are 10 temps)
+- a line with the fahrenheit to celcius conversion being set equal to the celcius temperatures array in position i
+- a line with fprintf command to write the new celcius temperature from your array position i into the temps_C file pointed variable
+- a line to add the temp in f from array position i to your f tempurature sum variable
+- a line to add the temp in c from array position i to your c tempurature sum variable
+
+- thats it inside the for loop, don't forget to close it with the closing brace
+
+## Last part calculating the average of each temperature and printing it onto the terminal
+
+- divide your f temp sum variable by 10 (since there are 10 temperatures total) and set it equal to your f temp average variable
+- do the same for your celcius temp sum setting it equal to your celcius temp average variable
+
+- use printf to print the average fahrenheit temp onto the screen
+- do the same to print the avg c temp onto the screen
+
+- Thats it! 
+    - make sure not to forget the closing brace at the end closing the int main function if it's not already there.
--- Work/TempConverterThroughtTxt/txtediting.c
+++ Work/TempConverterThroughtTxt/txtediting.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-int main(){
+int main(){ //opens main function
+    FILE *temps_F, *temps_C; //Initiates file pointers
+    temps_F = fopen("temps_F.txt", "r"); //opens the Fahrenheit file with mode: reading | then assigns it to the temps_F file pointer
+    temps_C = fopen("temps_C.txt", "w"); //creates the Celcius file with mode: writing (creates it and writes in it) | then assigns it to the temps_C file pointer
+    double tempF[10], newtempC[10], tempFsum,tempCsum, tempFavg, tempCavg; //declares 2 double arrays (tempF and newtempC) with 10 empty doubles (since we know there are 10 temps) | also declares 4 doubles, 2 for the sums of all c and f temps to calculate and assign to the 2 average doubles of c and f temps
 
-    FILE *temps_F, *temps_C;
-    temps_F = fopen("temps_F.txt", "r");
-    temps_C = fopen("temps_C.txt", "a+");
-    double tempF[10], newtempC[10], tempFsum,tempCsum, tempFavg, tempCavg;
-
-for(int i = 0; i <=9; i++){
-    fscanf(temps_F," %lf", &tempF[i]);
-    newtempC[i] = (double)(tempF[i]-32)*5/9;
-    fprintf(temps_C,"%.1lf\n", newtempC[i]);
-    tempFsum = tempFsum + tempF[i];
-    tempCsum = tempCsum + newtempC[i];
-}
-tempFavg = tempFsum/10;
-tempCavg = tempCsum/10;
-printf("The average temperature in Fahrenheit is: %.2lf\n", tempFavg);
-printf("The average temperature in Celcius is: %.2lf\n\n", tempCavg);
-}
+for(int i = 0; i <=9; i++){ //opens a for loop that --> (declares an integer i (which represents the item position in our arrays); says if i is less than or equal to 9 (since there are 10 temperatures); then add 1 to it and repeat)
+    fscanf(temps_F," %lf", &tempF[i]); //reads the file temps_F and scans it into our tempF array in the position i (which is 0-9 when cycled through the loop)
+    newtempC[i] = (double)(tempF[i]-32)*5/9; //uses math to convert the fahrenheit temp to celcius and assigns it to the array newtempC in position i (which is 0-9 when cycled through the loop)
+    fprintf(temps_C,"%.1lf\n", newtempC[i]); //writes the new converted celcius temperature in the temps_C txt file and starts a new line for the next temperature (since its in a loop)
+    tempFsum = tempFsum + tempF[i]; //adds the fahrenheit temp from position i in array tempF to tempFsum double (since its a loop it'll end with the sum of all the fahrenheit temps)
+    tempCsum = tempCsum + newtempC[i]; ////adds the celcius temp from position i in array tempC to tempCsum double (since its a loop it'll end with the sum of all the celcius temps)
+}//closes the for loop
+tempFavg = tempFsum/10; //divides the fahrenheit temps sum (tempFsum) by 10 to find the average and assigns it to the variable tempFavg
+tempCavg = tempCsum/10;//divides the celcius temps sum (tempCsum) by 10 to find the average and assigns it to the variable tempCavg
+printf("The average temperature in Fahrenheit is: %.2lf\n", tempFavg); //prints/outputs the average fahrenheit temperature in the terminal (from double tempFavg)
+printf("The average temperature in Celcius is: %.2lf\n\n", tempCavg); //prints/outputs the average celcius temperature in the terminal (from double tempCavg)
+} //closes main function
